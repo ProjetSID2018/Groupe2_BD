9ed21cea2750f008eb93abd96553f00cc505c53f
Aliounegahn <abbgahn@gmail.com>
2018-01-09 16:10:02 +0100

    declencheurs mot et mot_racine

--- sql/declencheurs/TMOT_RACINE.sql
+++ sql/declencheurs/TMOT_RACINE.sql
@@ -1,17 +1,12 @@
-﻿DELIMITER |
-CREATE TRIGGER TMOT_RACINE BEFORE INSERT
-ON MOT_RACINE FOR EACH ROW
-BEGIN
-    IF id_racine IS  NULL   
-      THEN
-        SELECT'La clé ne doit pas etre nulle' ;
-    END IF;
-    Select count(*) into nb1
-    from MOT_RACINE M ,MOT M1
-    where M.id_mot= M1.id_mot ;
-    IF nb1== 0   
-      THEN
-        SELECT'La clé étrangère n'existe pas' ;
-    END IF;
-END |
-DELIMITER ;
+﻿  DELIMITER |
+  CREATE TRIGGER TMOT_RACINE BEFORE INSERT
+  ON MOT_RACINE FOR EACH ROW
+  BEGIN
+       DECLARE CAPACITY_ERROR CONDITION FOR SQLSTATE '99991';  
+       SIGNAL CAPACITY_ERROR SET MESSAGE_TEXT = 'la clé ne peut pas etre nulle';
+       IF (id_racine is NULL) THEN
+		SIGNAL CAPACITY_ERROR;
+	   END IF;
+
+  END |
+  DELIMITER ;

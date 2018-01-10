5d04fefe62a3fd2ac46238c5aab224c26f8e72a3
Mickael-PC\Mickael <mickael.anjoran@orange.fr>
2018-01-10 12:26:33 +0100

    Mise à jour déclencheur TPOSITION_MOT Fonctionne !!

--- sql/declencheurs/TPOSITION_MOT.sql
+++ sql/declencheurs/TPOSITION_MOT.sql
@@ -1,13 +1,13 @@
 ﻿DELIMITER |
-CREATE TRIGGER TPOSITION_MOT BEFORE INSERT
-ON POSITION_MOT FOR EACH ROW
+CREATE TRIGGER Tposition_word BEFORE INSERT
+ON position_word FOR EACH ROW
 BEGIN
-    DECLARE nb1 int;
-    DECLARE nb2 int;
-    DECLARE nb3 int;
-    DECLARE nb4 int;
-    DECLARE nb5 int;
-    DECLARE nb6 int;
+    DECLARE nb1 INT;
+    DECLARE nb2 INT;
+    DECLARE nb3 INT;
+    DECLARE nb4 INT;
+    DECLARE nb5 INT;
+    DECLARE nb6 INT;
     DECLARE CAPACITY_ERROR CONDITION FOR SQLSTATE '99991';  
     DECLARE CLE_ETRANGERE1 CONDITION FOR SQLSTATE '99990';
     DECLARE CLE_ETRANGERE2 CONDITION FOR SQLSTATE '99992';
@@ -15,57 +15,63 @@ BEGIN
     DECLARE CLE_ETRANGERE4 CONDITION FOR SQLSTATE '99994';
     DECLARE CLE_ETRANGERE5 CONDITION FOR SQLSTATE '99995';
     DECLARE CLE_ETRANGERE6 CONDITION FOR SQLSTATE '99996';
-    SIGNAL CLE_ETRANGERE1 SET MESSAGE_TEXT = 'la cle etrangere liée au mot nexiste pas';
-    SIGNAL CLE_ETRANGERE2 SET MESSAGE_TEXT = 'la cle etrangere liée a lentité nexiste pas';
-    SIGNAL CLE_ETRANGERE3 SET MESSAGE_TEXT = 'la cle etrangere liée a pos_tagging nexiste pas';
-    SIGNAL CLE_ETRANGERE4 SET MESSAGE_TEXT = 'la cle etrangere liée a larticle nexiste pas';
-    SIGNAL CLE_ETRANGERE5 SET MESSAGE_TEXT = 'la cle etrangere liée au synonyme nexiste pas';
-    SIGNAL CLE_ETRANGERE6 SET MESSAGE_TEXT = 'la cle etrangere liée a wiki nexiste pas';
-    SIGNAL CAPACITY_ERROR SET MESSAGE_TEXT = 'la clé ne peut pas etre nulle';
+    SIGNAL CLE_ETRANGERE1 SET MESSAGE_TEXT = "Foreign key of Word doesn't exist";
+    SIGNAL CLE_ETRANGERE2 SET MESSAGE_TEXT = "Foreign key of Entity doesn't exist";
+    SIGNAL CLE_ETRANGERE3 SET MESSAGE_TEXT = "Foreign key of POS_TAGGING doesn't exist";
+    SIGNAL CLE_ETRANGERE4 SET MESSAGE_TEXT = "Foreign key of ARTICLE doesn't exist";
+    SIGNAL CLE_ETRANGERE5 SET MESSAGE_TEXT = "Foreign key of SYNONYME doesn't exist";
+    SIGNAL CLE_ETRANGERE6 SET MESSAGE_TEXT = "Foreign key of WIKI doesn't exist";
+    SIGNAL CAPACITY_ERROR SET MESSAGE_TEXT = "Key Cannot be empty";
     IF (id_position IS  NULL) THEN  
       SIGNAL CAPACITY_ERROR ;
     END IF;
 
-    Select count(*) into nb1
-    from POSITION_MOT M ,MOT M1
-    where M.id_mot= M1.id_mot ;
-    IF (nb1== 0)  THEN
-        SIGNAL CLE_ETRANGERE1 ;
+    SELECT count(PW.id_position) INTO nb1
+    FROM position_word PW, word W
+    WHERE PW.id_position = W.id_position;
+    IF (nb1 = 0) THEN
+        SIGNAL CLE_ETRANGERE1;
     END IF;
 
-    Select count(*) into nb2
-    from POSITION_MOT M ,entite M1
-    where M.id_entite= M1.id_entite ;
-    IF (nb2== 0 )  THEN
-        SIGNAL CLE_ETRANGERE2 ;
+    SELECT count(PW.id_entity) INTO nb2
+    FROM position_word PW, entity E
+    WHERE PW.id_entity = E.id_entity;
+    IF (nb2 = 0 ) THEN
+        SIGNAL CLE_ETRANGERE2;
     END IF;
 
-    Select count(*) into nb3
-    from POSITION_MOT M ,POS_TAGGING M1
-    where M.id_pos_tag= M1.id_pos_tag ;
-    IF (nb3== 0) THEN
-        SIGNAL CLE_ETRANGERE3 ;
+    SELECT count(PW.id_post_tag) INTO nb3
+    FROM position_word PW ,POS_TAGGING PT
+    WHERE PW.id_pos_tag = PT.id_pos_tag;
+    IF (nb3 = 0) THEN
+        SIGNAL CLE_ETRANGERE3;
     END IF;
 
-    Select count(*) into nb4
-    from POSITION_MOT M ,article M1
-    where M.id_article= M1.id_article ;
-    IF (nb4== 0) THEN
-        SIGNAL CLE_ETRANGERE4 ;
+    SELECT count(PW.id_article) INTO nb4
+    FROM position_word PW, article A
+    WHERE PW.id_article = A.id_article;
+    IF (nb4 = 0) THEN
+        SIGNAL CLE_ETRANGERE4;
     END IF;
     
-    Select count(*) into nb5
-    from POSITION_MOT M ,SYNONYME M1
-    where M.id_synonyme= M1.id_synonyme ;
-    IF (nb5== 0) THEN
-        SIGNAL CLE_ETRANGERE5 ;
+    SELECT count(PW.id_synonym) INTO nb5
+    FROM position_word PW ,SYNONYM S
+    WHERE PW.id_synonym = S.id_synonym;
+    IF (nb5 = 0) THEN
+        SIGNAL CLE_ETRANGERE5;
     END IF;
 
-    Select count(*) into nb6
-    from POSITION_MOT M ,wikiM1
-    where M.id_wiki= M1.id_wiki ;
-    IF (nb6== 0) THEN
-        SIGNAL CLE_ETRANGERE6 ;
+    SELECT count(PW.id_wiki) INTO nb6
+    FROM position_word PW ,wiki W
+    WHERE PW.id_wiki = W.id_wiki;
+    IF (nb6 = 0) THEN
+        SIGNAL CLE_ETRANGERE6;
     END IF;
 END |
 DELIMITER ;
+
+
+
+
+
+

2e9b5b066416489c72a49a07027f4a1c6ff94444
Unknown <dusart.alex@free.fr>
2018-01-11 14:43:17 +0100

    Update 2 stats

--- sql/procedures/procedure_update_mv_number_article_week_label.sql
+++ sql/procedures/procedure_update_mv_number_article_week_label.sql
@@ -11,10 +11,10 @@ BEGIN
    TRUNCATE mv_number_article_week_label;
 
    INSERT INTO mv_number_article_week_label
-   Select a.id_label, COUNT(a.id_article) as number_article
-    From appartient a, article ar
-    Where a.id_article = ar.id_article AND TO_DAYS(Date(Now()))-7 <= TO_DAYS(ar.date_publication) AND TO_DAYS(ar.date_publication) < TO_DAYS (Date(Now()))
-	GROUP BY a.id_label;
+   Select b.id_label, COUNT(b.id_article) as number_article
+    From belong b, article ar
+    Where b.id_article = ar.id_article AND TO_DAYS(Date(Now()))-7 <= TO_DAYS(ar.date_publication) AND TO_DAYS(ar.date_publication) < TO_DAYS (Date(Now()))
+	GROUP BY b.id_label;
 END |
 
 DELIMITER ;

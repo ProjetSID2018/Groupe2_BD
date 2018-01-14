cda55bc24690c5d1ba8151806fd40c803be3e115
Raphael <raphael.sourty@telegrafik.eu>
2018-01-14 15:41:30 +0100

    update semantic_pword

--- sql/Procedure SEMANTIC/SEMANTIC_PWORD.sql
+++ sql/Procedure SEMANTIC/SEMANTIC_PWORD.sql
@@ -41,11 +41,8 @@ CREATE PROCEDURE SEMANTIC_PWORD(IN v_id_article INT, IN v_position INT, IN v_wor
       SELECT LAST_INSERT_ID() INTO v_id_wiki;
     END IF ;
 
-
-
     SELECT position INTO v_position_up FROM position_word WHERE id_article = v_id_article AND position = v_position;
     
-
     #IF (v_position_up IS NULL) THEN
     #  INSERT INTO position_word(position,id_article,title,id_word,id_wiki)
     #  VALUES(v_position,v_id_article,false,v_id_word,v_id_wiki);

dc2911e5c59d1a39fd54f0a54359e85967c37a5b
Utilisateur <azzouza.william@gmail.com>
2018-01-14 16:12:27 +0100

    Update SEMANTIC_PWORD (version 2)

--- sql/Procedure SEMANTIC/SEMANTIC_PWORD.sql
+++ sql/Procedure SEMANTIC/SEMANTIC_PWORD.sql
@@ -22,25 +22,25 @@ CREATE PROCEDURE SEMANTIC_PWORD(IN v_id_article INT, IN v_position INT, IN v_wor
     FROM wiki
     WHERE file_wiki = v_file_wiki;
 
-
-
-    IF (v_id_synonym IS NULL AND v_synonym IS NOT NULL) THEN
+    # If the word doesn't exist, we can't add a synonym
+    IF (v_id_word IS NOT NULL AND v_id_synonym IS NULL AND v_synonym IS NOT NULL) THEN
       INSERT INTO synonym(id_synonym,synonym) VALUES(NULL,v_synonym);
       SELECT LAST_INSERT_ID() INTO v_id_synonym;
+    END IF ;
+
+    # If the word doesn't exist, we can't add a wiki
+    IF (v_id_word IS NOT NULL AND v_id_wiki IS NULL AND v_file_wiki IS NOT NULL) THEN
+      INSERT INTO wiki(id_wiki,file_wiki) VALUES(NULL,v_file_wiki);
+      SELECT LAST_INSERT_ID() INTO v_id_wiki;
     END IF ;
 
     IF (v_id_word IS NULL) THEN
       SELECT W.id_word  INTO v_id_word FROM position_word PW,word W
       WHERE PW.id_word = W.id_word AND PW.id_article = v_id_article
             AND PW.position = v_position;
-      UPDATE word SET id_synonym = v_id_synonym WHERE id_word = v_id_word;
     END IF;
 
-
-    IF (v_id_wiki IS NULL AND v_file_wiki IS NOT NULL) THEN
-      INSERT INTO wiki(id_wiki,file_wiki) VALUES(NULL,v_file_wiki);
-      SELECT LAST_INSERT_ID() INTO v_id_wiki;
-    END IF ;
+    UPDATE word SET id_synonym = v_id_synonym WHERE id_word = v_id_word;
 
     SELECT position INTO v_position_up FROM position_word WHERE id_article = v_id_article AND position = v_position;
 

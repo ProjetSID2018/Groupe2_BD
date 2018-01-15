8bfca450e1f94d604a0bafd066ec92ff0293fba7
LENOVO-PC\Jérémie <jeremiemaileonard@gmail.com>
2018-01-15 09:14:58 +0100

    Update procédure filtering_pauthor -> ajout d'une condition where sur le prénom

--- sql/Procedure FILTERING/FILTERING_PAUTHOR.sql
+++ sql/Procedure FILTERING/FILTERING_PAUTHOR.sql
@@ -11,14 +11,15 @@ CREATE PROCEDURE FILTERING_PAUTHOR (IN vid_article INT, IN v_surname_author VARC
 
 		SELECT a.id_author INTO vid_author
 		FROM author a
-		WHERE a.surname_author = v_surname_author;
+		WHERE a.surname_author = v_surname_author
+		AND a.firstname_author = v_firstname_author;
 
 		IF (vid_author IS NULL AND v_surname_author IS NOT NULL)  THEN
 			INSERT INTO author (id_author, surname_author, firstname_author) VALUES (NULL, v_surname_author,v_firstname_author);
 			SELECT LAST_INSERT_ID() INTO vid_author;
 		END IF;
 
-		INSERT INTO realize(id_author, id_article) VALUES(vid_author ,vid_article);
+		INSERT INTO realize(id_author, id_article) VALUES(vid_author, vid_article);
 
 		COMMIT;
 	END/

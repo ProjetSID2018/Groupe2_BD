f054dc8333d8ceb020d19093179b42d8b249e472
Utilisateur <azzouza.william@gmail.com>
2018-01-17 08:34:21 +0100

    COMMIT Mercredi 17/01 08h35 Update des noms des routes (version 2.2)

--- API/index/routes/web.php
+++ API/index/routes/web.php
@@ -7,15 +7,19 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 */
-Route::prefix('var/www/html/projet2018/code/bd_index/API/index')->group(function () {
+Route::prefix('API/index')->group(function () {
 
     // Label's routes
     Route::post('label', 'LabelController@store');
 
     // Filtering group's routes
-    Route::post('filtering', 'FilteringController@store')->name("code/bd_index/API/filtering");
+    Route::post('filtering', 'FilteringController@store');
 
     // Semantic group's routes
     Route::patch('semantic', 'SemanticController@store');
+
+    Route::get('test',function() {
+        return view('welcome');
+    });
 });
 
